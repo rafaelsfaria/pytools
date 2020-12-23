306c08aac33d658df25b823815dd32e9d0ae8ce1
Rafael Faria <rafaelsanfaria@gmail.com>
2020-12-22 21:46:03 -0300

    adiciona funcao buscar avatar no github

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def buscar_avatar(usuario):
+    """
+    Busca o avatar de um usuario no github
+    :param usuario: str com nome do usuario
+    :return: str com link do avatar
+    """
+    url=f"https://api.github.com/users/{usuario}"
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    link = buscar_avatar('rafaelsfaria')
+    print(link)
