e2cc68778ed7ff785dae77d0cd7f2479e2149029
Rafael Faria <rafaelsanfaria@gmail.com>
2021-02-17 20:58:49 -0300

    adiciona biblioteca pytest-mock para facilitar testes

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -5,7 +5,7 @@ import pytest
 from libpythonpro import github_api
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resultado_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/32245266?v=4'
     resultado_mock.json.return_value = {
@@ -13,11 +13,9 @@ def avatar_url():
         'avatar_url': url,
         'login': 'rafaelsfaria'
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resultado_mock)
-    url = github_api.buscar_avatar('rafaelsfaria')
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resultado_mock
+    return url
 
 def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('rafaelsfaria')
