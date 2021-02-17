43464b12d93702c5d1eb6c9ebcac8ea7be55f3aa
Rafael Faria <rafaelsanfaria@gmail.com>
2021-02-16 21:59:03 -0300

    adiciona email ao usuario dos testes

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,13 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='rafael')
+    usuario = Usuario(nome='rafael', email='rafael@email.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome='rafael'), Usuario(nome='rafael2')]
+    usuarios = [Usuario(nome='rafael1', email='rafael@email.com'), Usuario(nome='rafael2', email='rafael@email.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
